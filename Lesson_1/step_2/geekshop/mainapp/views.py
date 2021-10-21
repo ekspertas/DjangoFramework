07ef88d6c0f546114e2773b7be95d31e44a912c6
ekspertas <ekspertasd@gmail.com>
2021-10-21 18:40:30 +0300

    HW Lesson2

--- Lesson_1/step_2/geekshop/mainapp/views.py
+++ Lesson_1/step_2/geekshop/mainapp/views.py
@@ -4,12 +4,78 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'Главная'
+    }
+    return render(request, 'mainapp/index.html', context)
+
+
+def contact(request):
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'mainapp/contact.html', context)
+
+
+links_menu = [
+    {
+        'url': 'products',
+        'title': 'все'
+    },
+    {
+        'url': 'products_home',
+        'title': 'дом'
+    },
+    {
+        'url': 'products_office',
+        'title': 'офис'
+    },
+    {
+        'url': 'products_modern',
+        'title': 'модерн'
+    },
+    {
+        'url': 'products_classic',
+        'title': 'классика'
+    },
+]
     
 
 def products(request):
-    return render(request, 'mainapp/products.html')
-    
+    context = {
+        'links_menu': links_menu,
+        'title': 'Изделия'
+    }
+    return render(request, 'mainapp/products.html', context=context)
 
-def contact(request):
-    return render(request, 'mainapp/contact.html')
+
+def products_home(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Изделия для дома'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_office(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Изделия для офиса'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_modern(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Изделия модерн'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_classic(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Классические изделия'
+    }
+    return render(request, 'mainapp/products.html', context=context)
